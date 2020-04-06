ceb56ae15db7a5f3118d09381e888d1721bf38ac
SimKico <58364327+SimKico@users.noreply.github.com>
2020-04-06 14:09:09 +0200

    #01 Elementary demo

---
+++ eclinic-api/eclinic-api/src/main/java/com/eclinic/eclinicapi/EclinicApiApplication.java
@@ -0,0 +1,13 @@
+package com.eclinic.eclinicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclinicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclinicApiApplication.class, args);
+	}
+
+}
---
+++ eclinic-api/eclinic-api/src/test/java/com/eclinic/eclinicapi/EclinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eclinic.eclinicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclinicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
