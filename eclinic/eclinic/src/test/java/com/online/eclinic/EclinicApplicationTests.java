f89176826a858ce3f3e88a2b9874da1876e40af4
SimKico <58364327+SimKico@users.noreply.github.com>
2020-04-06 15:14:11 +0200

    #01 Izmjena elementnog dema
    Izgenerisan je novi pocetni projekar na stranici https://start.spring.io/ gdje je cekiran Dependencie Rest Repositories za razliku od proslog dema

---
+++ eclinic/eclinic/src/test/java/com/online/eclinic/EclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.eclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
