ae19f74068e1cda0ed98f4d1ac72904a8a70d06f
Snjezana Simic <s.snjezana@outlook.com>
2020-04-07 12:54:24 +0200

    #1 CORS enable

--- eclinicMRSISA/eclinic (1)/eclinic/src/main/java/com/mrsisa/eclinic/controller/EClinicController.java
+++ eclinicMRSISA/eclinic (1)/eclinic/src/main/java/com/mrsisa/eclinic/controller/EClinicController.java
@@ -1,19 +1,23 @@
 package com.mrsisa.eclinic.controller;
 
-import java.util.Collection;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/test")
 public class EClinicController {
 
-	@GetMapping("/hello")
-	public String sayHello() {
-		return "Hello world";
+	@CrossOrigin(origins = "http://localhost:3000")
+	@GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> sayHello() {
+		return new ResponseEntity<String> ("EClinic", HttpStatus.OK);
 	}
 	
 }
