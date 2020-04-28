450ac3b1f234a1ab59818a5e4d03c9844a6f75be
Snjezana Simic <s.snjezana@outlook.com>
2020-04-28 15:26:55 +0200

    #9 prelazak na jquery i ajax
    dodala fajl templates sa html
    povezala u kontroleru

--- eclinicMRSISA/eclinic (1)/eclinic/src/main/java/com/mrsisa/eclinic/controller/EClinicController.java
+++ eclinicMRSISA/eclinic (1)/eclinic/src/main/java/com/mrsisa/eclinic/controller/EClinicController.java
@@ -6,18 +6,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")// na jednom mjestu staviti na globalnom nivou
-@RequestMapping("/test")
+//@CrossOrigin(origins = "*", allowedHeaders = "*")// na jednom mjestu staviti na globalnom nivou
+//@RequestMapping()
 public class EClinicController {
 
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> sayHello() {
-		return new ResponseEntity<String> ("EClinic", HttpStatus.OK);
+//	@CrossOrigin(origins = "http://localhost:3000")
+//	@GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+//	public ResponseEntity<String> sayHello() {
+//		return new ResponseEntity<String> ("EClinic", HttpStatus.OK);
+//	}
+//	
+	
+	@RequestMapping(value = "/profilPacijenta")
+	public ModelAndView getData(){
+		ModelAndView newView = new ModelAndView();
+		newView.setViewName("profilPacijenta");
+			return newView;
 	}
 	
 }
