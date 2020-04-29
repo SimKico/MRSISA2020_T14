359bbf36afed40db759fe920edc13288baa8d962
Snjezana Simic <s.snjezana@outlook.com>
2020-04-29 15:18:40 +0200

    #9 povezani static fajlovi
    NAPOMENA!!! potrebno je u aplication properties promijeniti sifru  za bazu podataka tako da odgovara vasoj sifri

--- eclinicMRSISA/eclinic (1)/eclinic/src/main/java/com/mrsisa/eclinic/EclinicApplication.java
+++ eclinicMRSISA/eclinic (1)/eclinic/src/main/java/com/mrsisa/eclinic/EclinicApplication.java
@@ -2,6 +2,9 @@ package com.mrsisa.eclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EclinicApplication {
@@ -9,5 +12,15 @@ public class EclinicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EclinicApplication.class, args);
 	}
+	
+@Bean
+WebMvcConfigurer conf() {
 
+	return new WebMvcConfigurer() {
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+		}
+	};
+}
 }
