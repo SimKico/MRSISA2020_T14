1947f19c066c3d84db4dfe7b974de0ac7736cad8
Snjezana Simic <s.snjezana@outlook.com>
2020-06-13 22:20:50 +0200

    #14 uradjeno je logovanje korisnika i preusmjeravanje na njihove pocetne stranice. US#11

---
+++ eclinicMRSISA/eclinic (1)/eclinic/src/main/resources/static/js/logovanje.js
@@ -0,0 +1,41 @@
+var user = {};
+
+function logovanje()
+{
+	 user.email = document.getElementById("email").value;
+	 user.password = document.getElementById("lozinka").value;
+       console.log("before");
+       console.log(user.email);
+       console.log(user.password);
+       console.log("after");
+	$.ajax({
+		url:"/auth/login",
+		method :"POST",
+	    dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(user),
+        success: function(result) {
+        	console.log(result);
+        	console.log(result.accessToken);
+            localStorage.setItem("token", result.accessToken);
+            localStorage.setItem("email", result.email);
+            localStorage.setItem("expiresIn", result.expiresIn);
+            localStorage.setItem("firstName", result.firstName);
+            localStorage.setItem("type", result.type);
+            
+            if (result.type == "P") {
+            	location.replace("/homepagePacijent1.html");
+            } else if (result.type == "AC") {
+            	location.replace("/adminKlinikeHomepage.html");
+            } else if (result.type == "AK") {
+            	location.replace("/registracijaAdminaKlinika.html")
+            } else {
+            	location.replace("/index.html");
+            }
+            
+        }, error: function(error) {
+            toastr.error("Wrong email or password");
+            console.log(error);
+        }
+        });
+	}
